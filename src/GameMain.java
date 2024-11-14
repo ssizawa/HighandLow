e889d68b229ef3cc350437976e0ae9544622578d
ssizawa <izawa.shin@mikilab.doshisha.ac.jp>
2024-11-14 15:53:35 +0900

    create main

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,3 +1,22 @@
-public class GameMain {
-    
+// ゲームメインクラス
+public class GameMain
+{
+    public static void main(String[] args)
+    {
+        // 山札を生成
+        Deck deck = new Deck();
+
+        // 親がカードを引く
+        Player parent = new Player();
+        parent.Draw(deck);
+
+        // 子がカードを引く
+        Player child = new Player();
+        child.Draw(deck);
+
+        // ゲーム画面を生成
+        new Display( parent, child );
+
+        return;
+    }
 }
